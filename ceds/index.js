0cfe9b112066aeed0d01bd3622000da00d57728c
Tyler Hachadorian <49740237+hachadorian@users.noreply.github.com>
2024-01-11 09:06:30 -0500

    chart corrections and colors

--- ceds/index.js
+++ ceds/index.js
@@ -1,9 +1,10 @@
 const url = "https://dvrpc.github.io/cms-embedded-items/ceds/regions.xlsx";
 var file = await (await fetch(url)).arrayBuffer();
 var workbook = XLSX.read(file);
+const autocolors = window["chartjs-plugin-autocolors"];
+Chart.register(autocolors);
 
 var regionsMap = {
-  dvrpc: "DVRPC",
   ATL: "Atlanta",
   BAL: "Baltimore",
   BOS: "Boston",
@@ -15,12 +16,62 @@ var regionsMap = {
   WAS: "Washington",
 };
 
+var bubbleMap = {
+  0.2: "#c1e7ff",
+};
+
 var geographySelect = document.getElementById("geography");
-workbook.SheetNames.slice(1).map((name) => {
+workbook.SheetNames.slice(2, -1).map((name) => {
   var option = document.createElement("option");
   option.value = name;
   option.innerHTML = regionsMap[name];
   geographySelect.appendChild(option);
+});
+
+var dvrpcWorksheet = workbook.Sheets["dvrpc"];
+var dvrpc_data = XLSX.utils.sheet_to_json(dvrpcWorksheet, { header: 1 });
+dvrpc_data = dvrpc_data.filter((row) => parseInt(row[3]));
+var maxRadius = Math.max(...dvrpc_data.map((row) => row[3]));
+
+var dvrpcChart = new Chart(document.getElementById("bubble-dvrpc"), {
+  type: "bubble",
+  data: {
+    labels: dvrpc_data.map((row) => row[0]),
+    datasets: [
+      {
+        label: "null",
+        data: dvrpc_data.map((row) => ({
+          x: (row[1] * 100).toFixed(1),
+          y: (row[2] * 100).toFixed(1),
+          r: Math.round((row[3] / maxRadius) * 300),
+        })),
+      },
+    ],
+  },
+  options: {
+    maintainAspectRatio: false,
+    layout: {
+      autoPadding: false,
+    },
+    plugins: {
+      autocolors: {
+        mode: "data",
+      },
+      legend: {
+        display: false,
+      },
+      tooltip: {
+        callbacks: {
+          label: function (context) {
+            const row = dvrpc_data.filter((row) => row[0] === context.label)[0];
+            const total = `Employment: ${row[3].toLocaleString()}`;
+            const lq = `LQ: ${row[4]}`;
+            return [total, lq];
+          },
+        },
+      },
+    },
+  },
 });
 
 var chart;
@@ -55,6 +106,9 @@ function updateChart() {
         autoPadding: false,
       },
       plugins: {
+        autocolors: {
+          mode: "data",
+        },
         legend: {
           display: false,
         },
